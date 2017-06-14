d78ec6c4a6ddcf62659735cd5e8b6b14641d804d
Torsten Koschorrek <devel@koschorrek.com>
2017-06-14 21:36:21 +0200

    timer_functions: add function convert_countdown()
    Signed-off-by: Torsten Koschorrek <devel@koschorrek.com>

--- timer_functions.sh
+++ timer_functions.sh
@@ -16,3 +16,27 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# function convert_countdown
+#          convert countdown timer values to time and vice versa
+# param    $1: time value (format hh:mm:ss)
+#          $2: diff value (format hh:mm:ss), default 10 hours (10:00:00)
+# return   printf: calculated time (format hh:mm:ss)
+function convert_countdown() {
+    time_val=$1
+    diff_val=$2
+
+    if [ "$diff_val" == "" ]; then
+	diff_val="10:00:00"
+    fi
+
+    IFS=":" read -r t_hh t_mm t_ss <<< "$time_val"
+    IFS=":" read -r d_hh d_mm d_ss <<< "$diff_val"
+
+    result_val=$(( ($d_hh*60*60-$d_mm*60+$d_ss)-($t_hh*60*60+$t_mm*60+$t_ss) ))
+    r_hh=$(( $result_val/(60*60) ))
+    r_mm=$(( ($result_val-$r_hh*60*60)/60 ))
+    r_ss=$(( $result_val-$r_hh*60*60-$r_mm*60 ))
+
+    printf "%02d:%02d:%02d\n" "$r_hh" "$r_mm" "$r_ss"
+}
