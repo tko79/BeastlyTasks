1b7fba72545de787f66502863c5bd2a965578085
Torsten Koschorrek <devel@koschorrek.com>
2017-06-28 22:26:20 +0200

    timer_functions: use local instead of global variables
    Signed-off-by: Torsten Koschorrek <devel@koschorrek.com>

--- timer_functions.sh
+++ timer_functions.sh
@@ -24,6 +24,14 @@
 #          $3: "sum" or "sub"
 # return   printf: calculated time (format hh:mm:ss)
 function __sum_sub_time() {
+    local t1_hh=''
+    local t1_mm=''
+    local t1_ss=''
+    local t2_hh=''
+    local t2_mm=''
+    local t2_ss=''
+    local result_val=''
+
     IFS=":" read -r t1_hh t1_mm t1_ss <<< "$1"
     IFS=":" read -r t2_hh t2_mm t2_ss <<< "$2"
 
@@ -33,9 +41,9 @@ function __sum_sub_time() {
 	result_val=$(( (${t1_hh#0}*60*60+${t1_mm#0}*60+${t1_ss#0})-(${t2_hh#0}*60*60+${t2_mm#0}*60+${t2_ss#0}) ))
     fi
 
-    r_hh=$(( $result_val/(60*60) ))
-    r_mm=$(( ($result_val-$r_hh*60*60)/60 ))
-    r_ss=$(( $result_val-$r_hh*60*60-$r_mm*60 ))
+    local r_hh=$(( $result_val/(60*60) ))
+    local r_mm=$(( ($result_val-$r_hh*60*60)/60 ))
+    local r_ss=$(( $result_val-$r_hh*60*60-$r_mm*60 ))
 
     printf "%02d:%02d:%02d\n" "$r_hh" "$r_mm" "$r_ss"
 }
@@ -46,8 +54,8 @@ function __sum_sub_time() {
 #          $2: diff value (format hh:mm:ss), default 10 hours (10:00:00)
 # return   echo: calculated time (format hh:mm:ss)
 function convert_countdown() {
-    time_val=$1
-    diff_val=$2
+    local time_val=$1
+    local diff_val=$2
 
     if [ "$diff_val" == "" ]; then
 	diff_val="10:00:00"
@@ -62,8 +70,8 @@ function convert_countdown() {
 #          $2: time2 (format hh:mm:ss)
 # return   echo: calculated time (format hh:mm:ss)
 function sum_time() {
-    time1_val=$1
-    time2_val=$2
+    local time1_val=$1
+    local time2_val=$2
 
     echo $(__sum_sub_time $time1_val $time2_val "sum")
 }
@@ -74,13 +82,20 @@ function sum_time() {
 #          $2: until (format d:hh:mm)
 # return   printf: calculated time left (format mm)
 function time_left() {
-    now_val=$1
-    until_val=$2
+    local now_val=$1
+    local until_val=$2
+
+    local n_d=''
+    local n_hh=''
+    local n_mm=''
+    local u_d=''
+    local u_hh=''
+    local u_mm=''
 
     IFS=":" read -r n_d n_hh n_mm <<< "$now_val"
     IFS=":" read -r u_d u_hh u_mm <<< "$until_val"
 
-    result_val=$(( (${u_d#0}*24*60+${u_hh#0}*60+${u_mm#0})-(${n_d#0}*24*60+${n_hh#0}*60+${n_mm#0}) ))
+    local result_val=$(( (${u_d#0}*24*60+${u_hh#0}*60+${u_mm#0})-(${n_d#0}*24*60+${n_hh#0}*60+${n_mm#0}) ))
 
     printf "%d\n" "$result_val"
 }
@@ -91,10 +106,10 @@ function time_left() {
 #          $2: ntasks
 # return   printf: calculated time per task (format mm)
 function time_per_task() {
-    time_val=$1
-    ntasks_val=$2
+    local time_val=$1
+    local ntasks_val=$2
 
-    result_val=$(( time_val/ntasks_val ))
+    local result_val=$(( time_val/ntasks_val ))
 
     printf "%d\n" "$result_val"
 }
