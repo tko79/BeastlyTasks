3ad71bee9d10925339e3a67e0e9d185352c3d40d
Torsten Koschorrek <devel@koschorrek.com>
2017-07-18 01:02:19 +0200

    config_functions: set_config_counter(): add function
    Signed-off-by: Torsten Koschorrek <devel@koschorrek.com>

--- config_functions.sh
+++ config_functions.sh
@@ -134,3 +134,37 @@ function get_config_counter() {
        echo ${counter#counter=$counter_id;}
     fi
 }
+
+# function set_config_counter
+#          set counter to config
+# param    $1: config filename
+#          $2: unique id
+#          $3: description
+#          $4: start value
+#          $5: threshold
+#          $6: below or above is bad? (format: {below|above})
+#          $7: text, when counter is good
+#          $8: text, when counter equals threshold value
+#          $9: text, when counter is bad
+# return   return 1: in case of error (counter id not found)
+function set_config_counter() {
+    local configfile=$1
+    local counter_id=$2
+    local counter_description=$3
+    local counter_value=$4
+    local counter_threshold=$5
+    local counter_below_above=$6
+    local counter_desc_good=$7
+    local counter_desc_threshold=$8
+    local counter_desc_bad=$9
+    local counter_from_config=""
+
+    counter_from_config=$(grep "counter=$counter_id;" $configfile)
+    if [ $? == 1 ]; then
+	echo ""
+	return 1
+    else
+	sed -i "/counter=$counter_id;/d" $configfile
+	echo "counter="$counter_id";\""$counter_description"\";"$counter_value";"$counter_threshold";"$counter_below_above";\""$counter_desc_good\"";\""$counter_desc_threshold\"";\""$counter_desc_bad"\"" >> $configfile
+    fi
+}
