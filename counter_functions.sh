4c01409b446dfba0f0e4ed6036b997c595be6412
Torsten Koschorrek <devel@koschorrek.com>
2017-07-29 10:10:09 +0200

    counter_functions: add function set_counter_param()
    Signed-off-by: Torsten Koschorrek <devel@koschorrek.com>

--- counter_functions.sh
+++ counter_functions.sh
@@ -48,3 +48,49 @@ function get_counter_param() {
 	esac
     fi
 }
+
+# function set_counter_param
+#          wrapper to set counter parameter (value, description, ...)
+# param    $1: config filename
+#          $2: unique id
+#          $3: counter parameter
+#          $4: new value for parameter
+# return   return 1: in case of error (get_config_counter failed)
+function set_counter_param() {
+    local configfile=$1
+    local counter_id=$2
+    local counter_param=$3
+    local counter_newval=$4
+
+    local counter_from_config=""
+    local retval=0
+
+    counter_from_config=$(get_config_counter $configfile $counter_id)
+    if [ $? == 1 ]; then
+	echo ""
+	return 1
+    else
+	counter_from_config=${counter_from_config#counter=$counter_id;} | sed -e 's/\"//g'
+
+	local counter_description=$(echo    $counter_from_config | awk -F';' '{ print $1 }')
+	local counter_value=$(echo          $counter_from_config | awk -F';' '{ print $2 }')
+	local counter_threshold=$(echo      $counter_from_config | awk -F';' '{ print $3 }')
+	local counter_below_above=$(echo    $counter_from_config | awk -F';' '{ print $4 }')
+	local counter_desc_good=$(echo      $counter_from_config | awk -F';' '{ print $5 }')
+	local counter_desc_threshold=$(echo $counter_from_config | awk -F';' '{ print $6 }')
+	local counter_desc_bad=$(echo       $counter_from_config | awk -F';' '{ print $7 }')
+
+	case "$counter_param" in
+	    "description") counter_description=$counter_newval ;;
+	    "value")       counter_value=$counter_newval ;;
+	    "threshold")   counter_threshold=$counter_newval ;;
+	    "belabo")      counter_below_above=$counter_newval ;;
+	    "txtgood")     counter_desc_good=$counter_newval ;;
+	    "txtequal")    counter_desc_threshold=$counter_newval ;;
+	    "txtbad")      counter_desc_bad=$counter_newval ;;
+	esac
+
+	retval=$(set_config_counter $configfile $counter_id "$counter_description" $counter_value $counter_threshold "$counter_below_above" "$counter_desc_good" "$counter_desc_threshold" "$counter_desc_bad")
+	return $retval
+    fi
+}
