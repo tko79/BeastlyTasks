42fd9f0ed3b7caa14edab7412eefaa499cfc2b12
Torsten Koschorrek <devel@koschorrek.com>
2017-11-25 18:00:24 +0100

    task_functions: add function create_task_image()
    Signed-off-by: Torsten Koschorrek <devel@koschorrek.com>

--- task_functions.sh
+++ task_functions.sh
@@ -16,3 +16,32 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# function create_task_image
+#          create a task image with parameters for name, start date, end date
+#          and done marker.
+# param    $1: task name
+#          $2: start date (calender week)
+#          $3: end date (calender week)
+#          $4: done marker
+# return   <none>
+function create_task_image() {
+    name=$1
+    start=$2
+    end=$3
+    done=$4
+
+    template_red="/tmp/task_template_red.png"
+    template_done="/tmp/task_template_done.png"
+    outfile="/tmp/out.png"
+
+    convert $template_red -gravity Center -pointsize 20 -fill white \
+	-draw "text 0,-13 '"$name"'" \
+	-draw "text -60,20 '"$start"'" \
+	-draw "text 60,20 '"$end"'" \
+	$outfile
+
+    if [ $done -eq 1 ]; then
+	composite -gravity center $template_done $outfile $outfile
+    fi
+}
